7bd4a6f1750c06fcad33bcac5db31217765005cc
J. Tangelder <j.tangelder@gmail.com>
2012-12-14 06:51:52 -0800

    Create auto_comment.js

---
+++ auto_comment.js
@@ -0,0 +1,13 @@
+var comment = window.prompt("Enter your comment:");
+var links = document.querySelectorAll(".comment_link input");
+Array.prototype.forEach.call(links, function(l) { 
+  l.click();
+});
+
+setTimeout(function() { 
+  var textareas = document.querySelectorAll("textarea[name=add_comment_text_text]");
+  Array.prototype.forEach.call(textareas, function(t) { 
+    t.value = comment;
+    t.form.submit();
+  });
+}, 1000);
