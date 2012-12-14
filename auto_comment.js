410f05aeded26d18e82d77b4e5eb1bea9e89d191
J. Tangelder <j.tangelder@gmail.com>
2012-12-14 15:58:53 +0100

    Update auto_comment.js

--- auto_comment.js
+++ auto_comment.js
@@ -7,7 +7,8 @@ Array.prototype.forEach.call(links, function(l) {
 setTimeout(function() { 
   var textareas = document.querySelectorAll("textarea[name=add_comment_text_text]");
   Array.prototype.forEach.call(textareas, function(t) { 
-    t.value = comment;
+    t.value = comment;    
+    t.form.add_comment_text.value = comment;
     t.form.submit();
   });
 }, 1000);
